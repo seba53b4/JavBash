65958f9702bd8520f6028ac7eac5c7a5f93604ed
seba53b4 <seba5364@hotmail.com>
2020-04-21 16:06:55 -0300

    Conexion Initial

--- src/javbash/JavBash.java
+++ src/javbash/JavBash.java
@@ -8,36 +8,31 @@ package javbash;
 import com.jcraft.jsch.Session;
 import com.pastdev.jsch.DefaultSessionFactory;
 import com.pastdev.jsch.command.CommandRunner;
-
 import java.util.HashMap;
 import java.util.Map;
-
 
 // http://www.jcraft.com/jsch/examples de ayuda
 public class JavBash {
     public static void main(String[] args) throws Exception {
         
         DefaultSessionFactory sessionFactory = new DefaultSessionFactory(
-                "user", "10.24.135.44", 22
+                "malmira", "10.24.135.44", 22
         ); 
-        
         
         Map props = new HashMap<String, String>();
         props.put("StrictHostKeyChecking", "no");
         sessionFactory.setConfig(props);
-        sessionFactory.setPassword("lapassaca");
+        sessionFactory.setPassword("Movistar*159");
 
         CommandRunner runner = new CommandRunner(sessionFactory);
 
         String command = "ls -al";
         CommandRunner.ExecuteResult result = runner.execute(command);
-
         if (result.getStderr().isEmpty()) {
-            System.out.println(result.getStdout());
+            System.out.println(result.getStdout()+ "EMPTY");
         } else {
             System.out.println(result.getStderr());
         }
-
         runner.close();
     }
 }
